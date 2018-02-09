63f25d1f940b68b86cd94100f835c156ff4178c9
Paolo Scanferla <paolo.scanferla@gmail.com>
2018-02-09 07:31:03 +0200

    chore: make prettier.config.js files ES2017-compatible

--- static-server/prettier.config.js
+++ static-server/prettier.config.js
@@ -1,11 +1,5 @@
 const commonConfig = require("../prettier.config");
 
-module.exports = {
-    ...commonConfig,
-    overrides: [{
-        files: "test/**/*.ts",
-        options: {
-            printWidth: 120
-        }
-    }]
-};
+module.exports = Object.assign({}, commonConfig, {
+    overrides: [{ files: "test/**/*.ts", options: { printWidth: 120 } }]
+});
