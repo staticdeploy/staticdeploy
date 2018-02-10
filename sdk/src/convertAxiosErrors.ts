3e5f096cafcf06279cc9a90ec74e2ebe07ca6822
Paolo Scanferla <paolo.scanferla@gmail.com>
2018-02-10 21:36:21 +0200

    feat(sdk): improve errors thrown by StaticdeployClient's methods

---
+++ sdk/src/convertAxiosErrors.ts
@@ -0,0 +1,37 @@
+import { AxiosInstance } from "axios";
+import get = require("lodash/get");
+
+export class StaticdeployClientError extends Error {
+    public originalError: Error;
+    constructor(message: string, originalError: Error) {
+        super(message);
+        this.originalError = originalError;
+    }
+}
+
+function wrapMethod(method: (...args: any[]) => any) {
+    return async function(this: any) {
+        try {
+            return await method.apply(this, arguments);
+        } catch (err) {
+            const responseStatusCode = get(err, "response.status");
+            const responseErrorMessage = get(err, "response.data.message");
+            const message = responseStatusCode
+                ? responseErrorMessage
+                  ? `Error ${responseStatusCode}: ${responseErrorMessage}`
+                  : `Error ${responseStatusCode}`
+                : err.message;
+            throw new StaticdeployClientError(message, err);
+        }
+    };
+}
+
+export default function convertAxiosErrors(axios: AxiosInstance) {
+    axios.request = wrapMethod(axios.request);
+    axios.get = wrapMethod(axios.get);
+    axios.head = wrapMethod(axios.head);
+    axios.post = wrapMethod(axios.post);
+    axios.patch = wrapMethod(axios.patch);
+    axios.put = wrapMethod(axios.put);
+    axios.delete = wrapMethod(axios.delete);
+}
--- sdk/src/index.ts
+++ sdk/src/index.ts
@@ -1,6 +1,7 @@
-import Axios from "axios";
+import Axios, { AxiosInstance } from "axios";
 
 import AppsClient from "./AppsClient";
+import convertAxiosErrors from "./convertAxiosErrors";
 import DeploymentsClient from "./DeploymentsClient";
 import EntrypointsClient from "./EntrypointsClient";
 
@@ -10,22 +11,29 @@ export {
     IDeployment,
     IEntrypoint
 } from "@staticdeploy/storage";
+export { StaticdeployClientError } from "./convertAxiosErrors";
 
 export default class StaticdeployClient {
     public apps: AppsClient;
     public deployments: DeploymentsClient;
     public entrypoints: EntrypointsClient;
+    private axios: AxiosInstance;
 
-    constructor(config: { apiUrl: string; apiToken: string }) {
-        const axios = Axios.create({
+    constructor(config: { apiUrl: string; apiToken?: string }) {
+        this.axios = Axios.create({
             baseURL: config.apiUrl,
-            headers: {
-                Authorization: `Bearer ${config.apiToken}`
-            },
             withCredentials: true
         });
-        this.apps = new AppsClient(axios);
-        this.deployments = new DeploymentsClient(axios);
-        this.entrypoints = new EntrypointsClient(axios);
+        convertAxiosErrors(this.axios);
+        if (config.apiToken) {
+            this.setApiToken(config.apiToken);
+        }
+        this.apps = new AppsClient(this.axios);
+        this.deployments = new DeploymentsClient(this.axios);
+        this.entrypoints = new EntrypointsClient(this.axios);
+    }
+
+    setApiToken(apiToken: string) {
+        this.axios.defaults.headers.Authorization = `Bearer ${apiToken}`;
     }
 }
