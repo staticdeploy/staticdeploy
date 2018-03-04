b4aecc25828d3bb876fed0be88ea1c1402b3dba9
Paolo Scanferla <paolo.scanferla@gmail.com>
2018-03-04 23:11:20 +0200

    fix(app): authenticate StaticdeployClient on construction

--- app/src/common/staticdeployClient.ts
+++ app/src/common/staticdeployClient.ts
@@ -3,7 +3,10 @@ import StaticdeployClient from "@staticdeploy/sdk";
 import { API_URL } from "../config";
 import authTokenService from "./authTokenService";
 
-const staticdeployClient = new StaticdeployClient({ apiUrl: API_URL });
+const staticdeployClient = new StaticdeployClient({
+    apiUrl: API_URL,
+    apiToken: authTokenService.getStatus().authToken || undefined
+});
 
 authTokenService.onStatusChange(status => {
     staticdeployClient.setApiToken(status.authToken);
