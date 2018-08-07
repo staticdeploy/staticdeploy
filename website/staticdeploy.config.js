821055d3c430a2ca5371a284dd896b4322f5b7e6
pscanf <paolo.scanferla@gmail.com>
2018-08-07 22:27:31 +0200

    chore(website): deploy website using v0.9.0 features:
    - use caching headers on resources
    - use correct fallback status code 404
    - use config file for staticdeploy cli

---
+++ website/staticdeploy.config.js
@@ -0,0 +1,27 @@
+const { join } = require("path");
+
+const BUNDLE_TAG = process.env.CIRCLE_TAG || process.env.CIRCLE_BRANCH;
+const IS_MASTER = BUNDLE_TAG === "master";
+
+module.exports = {
+    bundle: {
+        from: join(__dirname, "/build/staticdeploy"),
+        name: "staticdeploy.io",
+        tag: BUNDLE_TAG,
+        description: `Commit ${process.env.CIRCLE_SHA1}`,
+        fallbackAssetPath: "/notFound.html",
+        fallbackStatusCode: 404,
+        headers: {
+            "**/*": {
+                "Cache-Control": "public, max-age=86400"
+            }
+        }
+    },
+    deploy: {
+        app: "staticdeploy.io",
+        entrypoint: IS_MASTER
+            ? "staticdeploy.io/"
+            : `staticdeploy.io/_/${BUNDLE_TAG}/`,
+        bundle: `staticdeploy.io:${BUNDLE_TAG}`
+    }
+};
