f3dd1973235e91ea87cf2f33190fd57d2aefdf26
pscanf <paolo.scanferla@gmail.com>
2019-09-09 09:16:04 +0300

    fix(staticdeploy): log all startup errors

--- staticdeploy/src/server.ts
+++ staticdeploy/src/server.ts
@@ -12,37 +12,38 @@ import setupAuthenticationStrategies from "./init/setupAuthenticationStrategies"
 import setupStorages from "./init/setupStorages";
 
 (async () => {
-    // Initialize components
-    const logger = getLogger(config);
-    const authenticationStrategies = getAuthenticationStrategies(
-        config,
-        logger
-    );
-    const managementRouter = await getManagementRouter(config);
-    const storagesModule = getStoragesModule(config, logger);
-    const expressApp = getExpressApp({
-        config,
-        authenticationStrategies,
-        logger,
-        managementRouter,
-        storagesModule,
-        usecases
-    });
-
-    // Run init functions
-    await setupAuthenticationStrategies(authenticationStrategies);
-    await setupStorages(storagesModule);
-    await createRootUserAndGroup(config, storagesModule.getStorages());
-
-    // Start the http server
+    let logger: ReturnType<typeof getLogger>;
     try {
+        // Initialize components
+        logger = getLogger(config);
+        const authenticationStrategies = getAuthenticationStrategies(
+            config,
+            logger
+        );
+        const managementRouter = await getManagementRouter(config);
+        const storagesModule = getStoragesModule(config, logger);
+        const expressApp = getExpressApp({
+            config,
+            authenticationStrategies,
+            logger,
+            managementRouter,
+            storagesModule,
+            usecases
+        });
+
+        // Run init functions
+        await setupAuthenticationStrategies(authenticationStrategies);
+        await setupStorages(storagesModule);
+        await createRootUserAndGroup(config, storagesModule.getStorages());
+
+        // Start the http server
         createServer()
             .on("request", expressApp)
             .listen(config.port, () => {
                 logger.info(`Server listening on port ${config.port}`);
             });
     } catch (err) {
-        logger.error(err, "Error boostrapping server");
+        logger!.error(err, "Error boostrapping server");
         process.exit(1);
     }
 })();
