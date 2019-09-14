7f9c0bb39863d0ad0ba0903ab589d46db8e1586f
pscanf <paolo.scanferla@gmail.com>
2019-09-14 11:17:09 +0300

    fix(management-console): fix fs casing

--- management-console/src/common/authService/IAuthStrategy.ts
+++
@@ -1,8 +0,0 @@
-export default interface IAuthStrategy {
-    name: string;
-    displayName: string;
-    init(): Promise<void>;
-    login(...params: any): Promise<void>;
-    logout(): Promise<void>;
-    getAuthToken(): Promise<string | null>;
-}
