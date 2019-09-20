4e9524bcd97e264e5dbab652618d9ffd8d4e0d0f
pscanf <paolo.scanferla@gmail.com>
2019-09-20 23:16:20 +0300

    feat(management-console): use login_hint in silent redirect

--- management-console/mock-server/currentUser/get.ts
+++ management-console/mock-server/currentUser/get.ts
@@ -1,7 +1,7 @@
 import { RequestHandler } from "express";
 
 export default ((_req, res) => {
-    if (Math.random() > 0.5) {
+    if (Math.random() > 0.9) {
         res.status(403).send({
             message:
                 "NoUserCorrespondingToIdpUserError: Access denied. To gain access, ask an admin to create a user with idp = idp and idpId = idpId"
